890c18b581a12075f0135904b02f7478841954f0
Adel M. Dawood <111702322+proadel@users.noreply.github.com>
2023-10-19 10:39:51 +0300

    Update readme.cpp

--- readme.cpp
+++ readme.cpp
@@ -1,29 +1,9 @@
-// Learn C++ Programming: Basics to Advanced
-/*
-1. Basic Syntax
-
-#include <iostream>
-// using namespace std;
-// main() is where program execution begins.
-// int main() {
+Learn C++ Programming: Basics to Advanced
+1. BasicSyntax
+   #include <iostream>
+using namespace std;
+// main() is where program execution begins. 
+int main() {
 // This is where you write your code
-//   return 0;
-}
-//This is the basic structure of C++. 
-//You will have to use this structure almost every time you write a C++ code. It contains the main function where the program execution begins.
-//
-  */
+return 0; }
 
-/*
-2. Output
-
-//cout << "Hello World";
-//cout prints anything under the “ ” to the screen.
-*/
-
-// 3. Input
-
-//int variable;
-
-//cin >> variable;
-//cin takes the input from the screen and stores it in the variable.
