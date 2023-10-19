30b231d00afda477b2bf96addbe9badff1343fb6
Adel M. Dawood <111702322+proadel@users.noreply.github.com>
2023-10-19 10:42:07 +0300

    Delete readme.cpp

--- readme.cpp
+++
@@ -1,9 +0,0 @@
-Learn C++ Programming: Basics to Advanced
-1. BasicSyntax
-   #include <iostream>
-using namespace std;
-// main() is where program execution begins. 
-int main() {
-// This is where you write your code
-return 0; }
-
